255487fbd75022a8c6cf8f52861dad380d412615
Bennett Jackson <bennettp.jackson@gmail.com>
2023-11-02 15:06:20 -0700

    created database classes and updated some source files

--- backend.java
+++ backend.java
@@ -1,11 +1,10 @@
 import java.util.Scanner; //this will be used to read in test data 
 //FIXME: if we are interfacing with other groups this will probably be removed
 
-import java.util.ArrayList; //this will be used for our mock databases
 public class backend {
     public static void main(String[] args) {
-        ArrayList <scholarship> scholarshipDatabase = new ArrayList<>(); //FIXME: array list of scholarship objects
-        ArrayList <user> userDatabase = new ArrayList<>(); //FIXME: user inheritance and make users objects
-
+        userDatabase userData = new userDatabase();
+        scholarshipDatabase scholarshipData = new scholarshipDatabase();
+        //This creates an object of each of our databases
     }
 }
--- scholarship.java
+++ scholarship.java
@@ -1,3 +1,31 @@
 public class scholarship {
+    private String scholarshipName;
+    private String customRequiredInfo;
+
+    public scholarship(){ //This is the constructor
+        scholarshipName = "No Name";
+        customRequiredInfo = "None";
+    }
+
+    ///////// Start mutators and accessors /////////
+
+    public void setScholarshipName(String name){
+        scholarshipName = name; //This sets the scholarship name
+    }
+
+    public String getScholarshipName(){
+        return scholarshipName; //This returns the scholarship name
+    }
+
+    public void setCustomRequiredInfo(String info){
+        customRequiredInfo = info; //This sets the shcolarship info
+    }
+
+    public String getCustomRequiredInfo(){
+        return customRequiredInfo; //This gets the scholarship info
+    }
+
+
+    ///////// End mutators and accessors /////////
     
 }
