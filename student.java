333316c8a41b0cdb9e44898db6784e37d38a2a87
Bennett Jackson <bennettp.jackson@gmail.com>
2023-11-03 00:19:23 -0700

    added in more of a template format that we will fill in and impliment later on. This is mainly to set up all the files we think we need right now so we have a good starting point.

--- student.java
+++ student.java
@@ -1,4 +1,24 @@
 //This student class inherits from the user class
 public class student extends user{
-    
+    private String major;
+    private int gpa;
+    private String studentID; //FIXME: do we want this as a string or int for comparisons
+    private String citizenship;
+    private int unitsEnrolled; //FIXME: do we want this as a string or int for comparisons
+    private String expectedGraduation;
+    private String essayResponses; //FIXME: may need a new database for this or we make an array of long strings here????
+    private String bursarAccountReference;
+    //FIXME: how do we want to do this ----->     private scholarship awardedScholarship;
+
+    public student(){//This is the default constructor for the student class
+        major = "None";
+        gpa = 0;
+        studentID = "None";
+        citizenship = "None";
+        unitsEnrolled = 0;
+        expectedGraduation = "None";
+        essayResponses = "None"; //FIXME: may need a new database for this or we make an array of long strings here????
+        bursarAccountReference = "None";
+    }
+    //FIXME: add in overloading
 }
--- user.java
+++ user.java
@@ -1,5 +1,18 @@
 //This is a parent class for all users
 //The specific users will be a child 
 public class user {
+    private String name;
+    private String emailAddress;
+    private String securityQuestion; //CSV of the 3 security questions that can be split later through .split() method
+    private String securityQuestionAnswers; //CSV of the 3 security questions that can be split later through .split() method
+
+    public user(){ //default constructor 
+        name = "No Name";
+        emailAddress = "None";
+        securityQuestion = "None";
+        securityQuestionAnswers = "None";
+    }
+
+    //FIXME: overload constructor
     
 }
