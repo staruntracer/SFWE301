45e437a48611a5272f74faf980b22c93b9ab3fc0
Bennett Jackson <bennettp.jackson@gmail.com>
2023-11-05 00:12:22 -0700

    Added overloaded constructors and getters/setters for most classes

--- scholarship.java
+++ scholarship.java
@@ -6,33 +6,83 @@ public class scholarship {
     private String preferedMajors;
 
     public scholarship(){ //This is the constructor
-        scholarshipName = "No Name";
-        payout = 0;
-        deadline = "N/A";
-        customRequiredInfo = "None";
-        preferedMajors = "None";
+        this.scholarshipName = "No Name";
+        this.payout = 0;
+        this.deadline = "N/A";
+        this.customRequiredInfo = "None";
+        this.preferedMajors = "None";
     }
-    //FIXME: add in overloading
+    
+    ///// Start Overloaded constructors for the scholarship class /////
+    public scholarship(String scholarshipName){
+        this.scholarshipName = scholarshipName;
+        this.payout = 0;
+        this.deadline = "N/A";
+        this.customRequiredInfo = "None";
+        this.preferedMajors = "None";
+    }
+    public scholarship(String scholarshipName, int payout){
+        this.scholarshipName = scholarshipName;
+        this.payout = payout;
+        this.deadline = "N/A";
+        this.customRequiredInfo = "None";
+        this.preferedMajors = "None";
+    }
+    public scholarship(String scholarshipName, int payout, String deadline){
+        this.scholarshipName = scholarshipName;
+        this.payout = payout;
+        this.deadline = deadline;
+        this.customRequiredInfo = "None";
+        this.preferedMajors = "None";
+    }
+    public scholarship(String scholarshipName, int payout, String deadline, String customRequiredInfo){
+        this.scholarshipName = scholarshipName;
+        this.payout = payout;
+        this.deadline = deadline;
+        this.customRequiredInfo = customRequiredInfo;
+        this.preferedMajors = "None";
+    }
+    public scholarship(String scholarshipName, int payout, String deadline, String customRequiredInfo, String preferedMajors){
+        this.scholarshipName = scholarshipName;
+        this.payout = payout;
+        this.deadline = deadline;
+        this.customRequiredInfo = customRequiredInfo;
+        this.preferedMajors = preferedMajors;
+    }
+    ///// End Overloaded constructors for the scholarship class /////
 
     ///////// Start mutators and accessors /////////
 
-    public void setScholarshipName(String name){
-        scholarshipName = name; //This sets the scholarship name
-    }
-
     public String getScholarshipName(){
-        return scholarshipName; //This returns the scholarship name
+        return scholarshipName;
     }
-
-    public void setCustomRequiredInfo(String info){
-        customRequiredInfo = info; //This sets the shcolarship info
+    public void setScholarshipName(String scholarshipName){
+        this.scholarshipName = scholarshipName;
     }
-
+    public int getPayout(){
+        return payout;
+    }
+    public void setPayout(int payout){
+        this.payout = payout;
+    }
+    public String getDeadline(){
+        return deadline;
+    }
+    public void setDeadline(String deadline){
+        this.deadline = deadline;
+    }
     public String getCustomRequiredInfo(){
-        return customRequiredInfo; //This gets the scholarship info
+        return customRequiredInfo;
     }
-
-
+    public void setCustomRequiredInfo(String customRequiredInfo){
+        this.customRequiredInfo = customRequiredInfo;
+    }
+    public String getPreferedMajors(){
+        return preferedMajors;
+    }
+    public void setPreferedMajors(String preferedMajors){
+        this.preferedMajors = preferedMajors;
+    }
     ///////// End mutators and accessors /////////
     
 }
